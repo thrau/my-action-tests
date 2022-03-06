e495d2d41b47342c3449f42f08e375a85a56e344
Thomas Rausch <thomas@thrau.at>
2022-03-06 18:12:07 +0100

    add build script

--- bin/multi-platform.sh
+++ bin/multi-platform.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-bash --version
+set -e
 
-echo "UNAME"
-uname
+function platform() {
+	case `uname` in
+		("Linux") echo "linux" ;;
+		("*_NT*") echo "win32" ;; # works for github actions runners
+		("Darwin") echo "darwin" ;;
+		(*) echo "unknown" ;;
+	esac
+}
 
-echo "UNAME -a"
-uname -a
+repo_url=https://github.com/localstack/localstack-packaged-cli
+src_dir=python_packages/localstack-packaged-cli
+vendor_dir=vendor/$(platform)/localstack-packaged-cli
+
+mkdir -p ${src_dir}
+mkdir -p ${vendor_dir}
+
+test -d ${src_dir}/.git || git clone ${repo_url} ${src_dir}
+(
+	cd ${src_dir}
+	make build
+)
+cp -r ${src_dir}/dist/localstack ${vendor_dir}
+
+ls ${vendor_dir}/localstack
